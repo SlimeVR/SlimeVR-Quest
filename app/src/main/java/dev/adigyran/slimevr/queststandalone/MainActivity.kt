2d7ceac42a35b33aeb4eb0e3e4f9b6a134e483bf
adigyran <adigyran@gmail.com>
2021-10-02 18:13:46 +0300

    removed test foreground service

--- app/src/main/java/dev/adigyran/slimevr/queststandalone/MainActivity.kt
+++ app/src/main/java/dev/adigyran/slimevr/queststandalone/MainActivity.kt
@@ -10,7 +10,6 @@ import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import dev.adigyran.slimevr.queststandalone.standalone.databinding.ActivityMainBinding
-import dev.adigyran.slimevr.queststandalone.testservice.TestForegroundService
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,46 +20,12 @@ class MainActivity : AppCompatActivity() {
         binding = provideViewBinding(layoutInflater)
         val view = binding.root
         setContentView(view)
-        LocalBroadcastManager.getInstance(this)
-            .registerReceiver(logReceiver, IntentFilter("TIMR"))
-        startTestService()
-
-
     }
 
     private fun provideViewBinding(layoutInflater: LayoutInflater): ActivityMainBinding  = ActivityMainBinding.inflate(layoutInflater)
 
-    private fun startTestService()
-    {
-
-        val testServiceIntent = Intent(this, TestForegroundService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            this.startForegroundService(testServiceIntent)
-        }
-        else
-        {
-            this.startService(testServiceIntent)
-        }
-    }
-
-    private val logReceiver: BroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            when (intent.action) {
-                "TIMR" -> {
-                    val timerValue = intent.getStringExtra("VLU")
-                    runOnUiThread { binding.numbr.text = timerValue }
-
-
-                  return
-                }
-            }
-        }
-    }
 
     override fun onDestroy() {
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(logReceiver)
-        val intent = Intent("kill-ze-service")
-        this.sendBroadcast(intent)
         super.onDestroy()
 
     }
